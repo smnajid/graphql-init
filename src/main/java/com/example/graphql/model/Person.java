0136d01c896fa146d128976ed5c964c4c5e952f4
Mohamed NAJID <mohamed.najid@gmail.com>
2020-10-06 23:39:03 +0200

    First commit

---
+++ src/main/java/com/example/graphql/model/Person.java
@@ -0,0 +1,14 @@
+package com.example.graphql.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
